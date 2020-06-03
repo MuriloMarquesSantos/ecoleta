6e18df914fa4ae57bb93ac02e8f64d2fa3319132
MuriloMarquesSantos <murilommms@gmail.com>
2020-06-03 20:26:59 -0300

    Adding cors to application

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.listen(3333);
 app.use(express.json());
 app.use(routes);
