1ab38402627150ba0b92170645301e0318088eb6
MuriloMarquesSantos <murilommms@gmail.com>
2020-06-05 18:47:48 -0300

    Adding city and UF states and integrating with ibge api to gather this info

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import api from '../../services/api';
+import ibgeApi from '../../services/ibgeApi'
 
 import './styles.css';
 
@@ -13,16 +14,59 @@ interface Item {
     image_url: string
 }
 
+interface IBGEUFResponse {
+    sigla: string
+}
+
+interface City {
+    id: number,
+    nome: string
+}
+
 const CreatePoint = () => {
     const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<City[]>([]);
+    const [selectedUf, setSelectedUf] = useState("");
+    const [selectedCity, setSelectedCity] = useState("");
+    // const [selectedItem, setSelectedItem] = useState(0);
 
     useEffect(() => {
         const getItems = async () => {
             const response = await api.get('/items');
             setItems(response.data);
         }
+
+        const getUfs = async () => {
+            const response = await ibgeApi.get<IBGEUFResponse[]>('/estados');
+            const ufInitials = response.data.map(uf => uf.sigla);
+            setUfs(ufInitials);
+        }
         getItems();
+        getUfs();
     }, [])
+
+    useEffect(() => {
+        const getCities = async () => {
+            const response = await ibgeApi.get(`/estados/${selectedUf}/municipios`);
+            setCities(response.data);
+        }
+        if (selectedUf) {
+            getCities();
+        }
+    }, [selectedUf]);
+
+    function handleSelectUfChange(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedUf(event.target.value);
+    }
+
+    function handleSelectCityChange(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedCity(event.target.value);
+    }
+
+    // function handleClickItemChange(event: ChangeEvent<HTMLLIElement> ) {
+    //     setSelectedItem(event.target.value)
+    // }
 
     return (
         <div id="page-create-point">
@@ -72,14 +116,24 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
-                                <option value="0"> Selecione uma UF</option>
+                            <select name="uf" id="uf" onChange={handleSelectUfChange}>
+                                <option value="0" >Selecione uma UF</option>
+                                {
+                                    ufs.map(uf => {
+                                        return <option key={uf} value={uf}>{uf}</option>
+                                    })
+                                }
                             </select>
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select name="city" id="city" onChange={handleSelectCityChange}>
                                 <option value="0"> Selecione uma cidade</option>
+                                {
+                                    cities.map(city => {
+                                        return <option key={city.id} value={city.nome}>{city.nome}</option>
+                                    })
+                                }
                             </select>
                         </div>
                     </div>
@@ -96,7 +150,7 @@ const CreatePoint = () => {
                             items.map(item => {
                                 return (
                                     <li key={item.id}>
-                                        <img src={item.image_url} alt={item.title}/>
+                                        <img src={item.image_url} alt={item.title} />
                                         <span>{item.title}</span>
                                     </li>
                                 )
